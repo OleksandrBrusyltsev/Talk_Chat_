93b343045eb12de8f77c27d082e7b2f3bef3255e
Alex <124603897+OleksandrBrusyltsev@users.noreply.github.com>
2023-10-17 12:10:35 +0300

    add socket settings

--- talk_app/authentication/constans.py
+++ talk_app/authentication/constans.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from pytz import timezone
+
+
 DEFAULT_PROFILE_PHOTO_FILENAME = '/profile_photos/default_profile_photo.jpg'
 
 FRONT_URL = 'https://valerka4052.github.io/chat-talk-front/login/'
@@ -6,3 +10,8 @@ RESET_PASSWORD_URL = 'https://valerka4052.github.io/chat-talk-front/recover-pass
 
 FROM_EMAIL = 'talk.team.challenge@gmail.com'
 
+cloudinary_base_url = "https://res.cloudinary.com/dzbm3urzv/image/upload/"
+
+
+
+kiev = timezone('Europe/Kiev')
--- talk_app/chat/consumers.py
+++ talk_app/chat/consumers.py
@@ -1,3 +1,4 @@
+from authentication.constans import cloudinary_base_url, kiev
 from chat.models import Message
 from channels_main.models import Channel
 from authentication.models import CustomUser
@@ -7,7 +8,8 @@ from rest_framework_simplejwt.tokens import UntypedToken
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from django.http import QueryDict
 import json
-
+from datetime import datetime
+from pytz import timezone
 
 @database_sync_to_async
 def create_message(text, user, channel):
@@ -16,23 +18,22 @@ def create_message(text, user, channel):
         owner=user,
         channel=channel
     )
-
-
+####
 @database_sync_to_async
 def get_user(user_id):
     return CustomUser.objects.get(id=user_id)
 
-
 @database_sync_to_async
 def get_messages(channel):
     return list(Message.objects.filter(channel=channel).values(
-        'text', 'owner__username', 'created_at'))
+        'id', 'text', 'owner__id', 'owner__profile_photo', 'owner__username', 'owner__email', 'created_at'
+    ))
+
 
 
 @database_sync_to_async
 def get_channel(channel_id):
     return Channel.objects.get(id=channel_id)
-
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -71,9 +72,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         messages = await get_messages(self.channel)
 
         for message in messages:
+            full_profile_photo_url = cloudinary_base_url + message['owner__profile_photo'] if message[
+                'owner__profile_photo'] else None
             await self.send(text_data=json.dumps({
+                'message_id': message['id'],  # ID сообщения
+                'user_id': message['owner__id'],  # ID пользователя
+                'profile_photo': full_profile_photo_url,  # фото пользователя
                 'message': message['text'],
                 'username': message['owner__username'],
+                'email': message['owner__email'],
                 'created_at': message['created_at'].strftime('%Y-%m-%d %H:%M:%S'),
             }))
 
@@ -89,11 +96,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         user = self.scope['user']
 
         message = await create_message(message_text, user, self.channel)
+        full_profile_photo_url = cloudinary_base_url + (user.profile_photo.url if user.profile_photo else "")
 
         await self.channel_layer.group_send(
             str(self.channel.id),
             {
                 'type': 'chat_message',
+                'message_id': message.id,
+                'user_id': user.id,
+                'profile_photo': user.profile_photo.url,  # фото пользователя
                 'message': message.text,
                 'username': user.username,
                 'email': user.email,
@@ -101,11 +112,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
+
+
+
+
     async def chat_message(self, event):
+
+        utc_time_str = event['created_at']
+        utc_time = datetime.strptime(utc_time_str, '%Y-%m-%d %H:%M:%S')
+        utc_time = timezone('UTC').localize(utc_time)
+        kiev_time = utc_time.astimezone(kiev)
+        kiev_time_str = kiev_time.strftime('%Y-%m-%d %H:%M:%S')
+
+
+
         await self.send(text_data=json.dumps({
             'message': event['message'],
             'username': event['username'],
             'email': event['email'],
-            'created_at': event['created_at'],
-            'type': 'sender'
+            'created_at': kiev_time_str,
+            'type': 'sender',
+            'user_id': event.get('user_id', None),
+            'message_id': event.get('message_id', None),
+            'profile_photo': event.get('profile_photo', None),
         }))
